5aa848b215760ce27667e8e7396ee9646f1ef288
Melissa Mepham <m.a.mepham@gmail.com>
2018-04-04 20:51:13 +1200

    starting to create a new object with prices

--- calculator.js
+++ calculator.js
@@ -6,11 +6,35 @@ module.exports = {
 
 function calculator (base, cart) {
   let priceObject = {}
-  let optionsObj = {}
       base.filter((item, i) => {
         item['product-type'] === ['product-type']
-        return priceObject[item['product-type']] = optionsObj
-        }
-      )
-      console.log(priceObject)
+        return priceObject[item['product-type']] = {}
+        })
+        optionsObjFunc(base, cart, priceObject)
+        console.log(priceObject)
+}
+
+function optionsFunc(base, cart, priceObject) {
+  for (i = 0; i < base.length; i ++) {
+    console.log(base[i].options.colour)
+
+  }
+
+}
+
+
+function optionsObjFunc(base, cart, optionsObj) {
+  base.filter((item, i) => {
+    if (item['product-type'] === 'hoodie') {
+      console.log(optionsObj.hoodie[item['base-price']] = ('hoodie' + item.options.colour + item.options.size))
+    } else if (item['product-type'] === 'sticker') {
+      console.log(optionsObj.sticker[item['base-price']] = ('sticker' + item.options.size[0]))
+    } else if (item['product-type'] === 'leggings'){
+      console.log(optionsObj.leggings[item['base-price']] = ('leggings'))
+
+    }
+
+    // console.log(item.options.colour[0])
+    }
+  )
 }
