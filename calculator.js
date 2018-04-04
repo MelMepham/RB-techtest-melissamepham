da6bfd5a5e301eddd6ad8fcd08f557089145f632
Melissa Mepham <m.a.mepham@gmail.com>
2018-04-04 23:06:33 +1200

    trying again with reduce JS

--- calculator.js
+++ calculator.js
@@ -1,40 +1,19 @@
-let fs = require('fs')
+var fs = require('fs')
 
-module.exports = {
-  calculator: calculator
-}
+const base = JSON.parse(fs.readFileSync('./data/basePrices.json', 'utf8'))
+const cart = JSON.parse(fs.readFileSync('./data/cart.json', 'utf8'))
 
-function calculator (base, cart) {
-  let priceObject = {}
-      base.filter((item, i) => {
-        item['product-type'] === ['product-type']
-        return priceObject[item['product-type']] = {}
-        })
-        optionsObjFunc(base, cart, priceObject)
-        console.log(priceObject)
-}
+// module.exports = {
+//   calculator,
+// }
+console.log(base)
+const output = base
+  .reduce((typesWithPrices, base) => {
+    typesWithPrices[base['product-type']] = typesWithPrices[base['product-type']] || [];
+    typesWithPrices[base['product-type']].push({
+      [base.options.colour + base.options.size]: base['base-price']
+    });
+    return typesWithPrices
+  }, {})
 
-function optionsFunc(base, cart, priceObject) {
-  for (i = 0; i < base.length; i ++) {
-    console.log(base[i].options.colour)
-
-  }
-
-}
-
-
-function optionsObjFunc(base, cart, optionsObj) {
-  base.filter((item, i) => {
-    if (item['product-type'] === 'hoodie') {
-      console.log(optionsObj.hoodie[item['base-price']] = ('hoodie' + item.options.colour + item.options.size))
-    } else if (item['product-type'] === 'sticker') {
-      console.log(optionsObj.sticker[item['base-price']] = ('sticker' + item.options.size[0]))
-    } else if (item['product-type'] === 'leggings'){
-      console.log(optionsObj.leggings[item['base-price']] = ('leggings'))
-
-    }
-
-    // console.log(item.options.colour[0])
-    }
-  )
-}
+console.log(JSON.stringify(output, null, 2))
